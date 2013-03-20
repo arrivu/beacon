4466b9a275b272bed87bf3fb01bb91bb4a0584d2
samuelrails <samuels410@gmail.com>
2013-03-20 19:36:48 +0530

    add api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  if Rails.env.production?
    provider :facebook, '153576101469926', '302358063a941b9e0d9c8ec9cb77b6ad'
-   provider :linkedin, 'gl1h6zyltlem', "wIfucZImlYGqFO3r"
+   provider :linkedin, 'z1zu2sg48rvp', "8kpRB8V5EBsiXcbx"
    provider :google_oauth2, '928801233183-phpisd2fada5c2d5n5heosha9h8jisla.apps.googleusercontent.com', 'D6mLnz02jOX6kpT-cbo9p2MM'
    
  elsif Rails.env.development?
