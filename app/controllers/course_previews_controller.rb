40fa500209c02fdb44070168775d2c3c88459598
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-04-12 14:33:21 +0530

    course pricings

--- app/controllers/course_previews_controller.rb
+++ app/controllers/course_previews_controller.rb
@@ -21,6 +21,7 @@ before_filter :check_admin_user, :only => [:new,:create, :edit, :destroy,:index]
 	def index
 		@previews = CoursePreview.paginate(page: params[:page], :per_page => 10)
 		@course = Course.all
+		
 	end
 
 	def edit
