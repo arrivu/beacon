0529f3f4c4fe82110523fbee9eb6f8e76ad1d1cc
RamakrishnanK <ramakrishnan@arrivusystems.com>
2013-04-12 12:07:27 +0530

    issues correction

--- app/models/course_preview.rb
+++ app/models/course_preview.rb
@@ -2,15 +2,17 @@ class CoursePreview < ActiveRecord::Base
 
 	attr_accessible :desc, :enable, :name, :sequence, :video_url, :course_id
 	belongs_to :course
-	validates :desc, presence: true, length: { maximum: 50 }
+	
 
 	validates :name, presence: true, length: { maximum: 15 }
+	validates :desc, presence: true, length: { maximum: 50 }
 	validates :sequence, presence: true, :numericality => {:only_integer => true},length: { maximum: 3 } 
 	validates :course_id, presence: true
 
 	HUMANIZED_ATTRIBUTES = {
 		:sequence => "Display Order",
-		:desc => "Description"
+		:desc => "Description",
+		:name=> "Title"
 	}
 
 	def self.human_attribute_name(attr, options={})
