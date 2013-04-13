535642c01a97fa36524b881b635b8b5505f5a541
natarajan <natarajan@arrivusystem.com>
2013-04-13 15:00:06 +0530

    tax rate date  validation done

--- app/helpers/tax_rates_helper.rb
+++ app/helpers/tax_rates_helper.rb
@@ -1,2 +1,14 @@
 module TaxRatesHelper
+	def nooverlap?(tax_all, start_date2,end_date2)
+		#logger=Logger.new("test")
+		tax_all.each do |taxc|
+			start_date1 = Date.parse(taxc.valid_from.to_s)
+			#logger.debug("fromdate"+ start_date1.to_s)
+			end_date1 = Date.parse(taxc.valid_until.to_s)
+			start_date2 = Date.parse(start_date2.to_s)
+			end_date2 = Date.parse(end_date2.to_s)
+			#logger.debug ((start_date1..end_date1).to_a & (start_date2..end_date2).to_a)
+			return unless ((start_date1..end_date1).to_a & (start_date2..end_date2).to_a).empty?
+		end
+	end
 end
