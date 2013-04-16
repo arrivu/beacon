ff34dffc96369f16116ae58b59fbda37189bdc7a
mani <mani.narayanan@arrivusystems.com>
2013-04-16 10:01:47 +0530

    Teaching staff create method impl

--- app/controllers/teaching_staffs_controller.rb
+++ app/controllers/teaching_staffs_controller.rb
@@ -1,45 +1,66 @@
-		class TeachingStaffsController < ApplicationController
-			before_filter :admin_user?, :only => [:new, :create]
-		  after_filter :teaching_staff_create, :only => [:create]
-		  protect_from_forgery :except => :create
-			
-		 def new
-		 	 @teachingstaff = TeachingStaff.new
-       @teachingstaff.build_user
-       #respond_with(@teachingstaff)	
-       render 'new' 
+class TeachingStaffsController < ApplicationController
+	before_filter :admin_user?, :only => [:new, :create]	
+	protect_from_forgery :except => :create
 
-			end
+	def new
+		@teachingstaff = TeachingStaff.new
+		@teachingstaff.build_user
+		render 'new' 
+	end
 
-			def create			
-				
-				@teachingstaff=TeachingStaff.new(params[:teaching_staff])
-				if @teachingstaff.save
-					flash[:notice]="Teaching Staff details created successfully"
-					redirect_to new_teaching_staff_path
-				else
-					render 'new'
-				end			
+	def create
+
+		@teachingstaff = TeachingStaff.new
+		@teachingstaff.name =  params[:teaching_staff][:teachingStaff_user][:name]
+		@teachingstaff.description =  params[:teaching_staff][:description]
+		@teachingstaff.qualification =  params[:teaching_staff][:qualification]
+		@teachingstaff.build_user(name: params[:teaching_staff][:teachingStaff_user][:name],
+								email: params[:teaching_staff][:teachingStaff_user][:email],
+								password: params[:teaching_staff][:teachingStaff_user][:password],
+								password_confirmation: params[:teaching_staff][:teachingStaff_user][:password_confirmation])
+		@teachingstaff.save
+		render :new
+
+		# @user = User.new
+		# @user.name = params[:teaching_staff][:teachingStaff_user][:name]
+		# @user.email = params[:teaching_staff][:teachingStaff_user][:email]
+		# @user.password = params[:teaching_staff][:teachingStaff_user][:password]
+		# @user.password_confirmation = params[:teaching_staff][:teachingStaff_user][:password_confirmation]		
+		# User.transaction do
+  # 		if @user.save
+  # 			@teachingstaff.save
+  # 		end
+		# end
+	end
+
+	# def create
+	# 	@teachingstaff=TeachingStaff.new(params[:teaching_staff])
+	# 	if @teachingstaff.save
+	# 		flash[:notice]="Teaching Staff details created successfully"
+	# 		redirect_to new_teaching_staff_path
+	# 	else
+	# 		render 'new'
+	# 	end			
+	# end
+
+	def edit
+		@teachingstaff=TeachingStaff.find(params[:id])
+	end
+
+	def update
+		@teachingstaff=TeachingStaff.find(params[:id])
+		if @teachingstaff.update_attributes(params[:teaching_staff])
+			flash="Teaching Staff details updated successfully"
+			redirect_to @teachingstaff
+		else
+			render 'edit'
 		end
-			
-			def edit
-				@teachingstaff=TeachingStaff.find(params[:id])
-			end
-			def update
-				@teachingstaff=TeachingStaff.find(params[:id])
-				if @teachingstaff.update_attributes(params[:teaching_staff])
-					flash="Teaching Staff details updated successfully"
-					redirect_to @teachingstaff
-				else
-					render 'edit'
-				end
-			end
-			def destroy
-				@teachingstaff=TeachingStaff.find(params[:id])
-				@teachingstaff.destroy
-				redirect_to teachingstaffs_path
-				flash[:notice] = "Deleted teaching staff details successfully"
-			end
-			
+	end
 
-		end
+	def destroy
+		@teachingstaff=TeachingStaff.find(params[:id])
+		@teachingstaff.destroy
+		redirect_to teachingstaffs_path
+		flash[:notice] = "Deleted teaching staff details successfully"
+	end
+end
