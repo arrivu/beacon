03c44b02a37dc16d892298c9f8810dbafeaf1064
natarajan <natarajan@arrivusystem.com>
2013-04-18 12:21:42 +0530

    date validation for Course Pricing

--- app/helpers/course_pricings_helper.rb
+++ app/helpers/course_pricings_helper.rb
@@ -1,2 +1,14 @@
 module CoursePricingsHelper
+	def nooverlap?(course_ids,start_date,end_date)
+		course_ids.each do |course|
+			start_date1=Date.parse(course.start_date.to_s)
+			end_date1=Date.parse(course.end_date.to_s)
+			start_date=Date.parse(start_date.to_s)
+			end_date=Date.parse(end_date.to_s)
+			return unless((start_date1..end_date1).to_a & (start_date..end_date).to_a).empty?
+		end
+	end
 end
+
+
+
