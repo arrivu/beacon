ffb3cf40b32b0dc5e648cb1e06b31564cb54024f
sukumar <sukumar@arrivusystems.com>
2013-04-18 15:09:53 +0530

    add teaching staff image

--- app/controllers/teaching_staffs_controller.rb
+++ app/controllers/teaching_staffs_controller.rb
@@ -18,6 +18,8 @@ class TeachingStaffsController < ApplicationController
 		@teachingstaff.qualification =  params[:teaching_staff][:qualification]
 		@teachingstaff.build_user(name: params[:teaching_staff][:teaching_staff_user][:name],
 								email: params[:teaching_staff][:teaching_staff_user][:email],
+								content_type: params[:teaching_staff][:teaching_staff_user][:content_type],
+								attachment: params[:teaching_staff][:teaching_staff_user][:attachment],
 								password: params[:teaching_staff][:teaching_staff_user][:password],
 								password_confirmation: params[:teaching_staff][:teaching_staff_user][:password_confirmation])
 		if @teachingstaff.save
@@ -52,6 +54,8 @@ class TeachingStaffsController < ApplicationController
 		if @teachingstaff.user.update_attributes(:email => params[:teaching_staff][:user][:email],
 									password: params[:teaching_staff][:user][:password],
 									password_confirmation: params[:teaching_staff][:user][:password_confirmation],
+  								content_type: params[:teaching_staff][:user][:content_type],
+	  							attachment: params[:teaching_staff][:user][:attachment],
 									name:params[:teaching_staff][:user][:name]) && @teachingstaff.update_attributes(
 									description:params[:teaching_staff][:description],
 									qualification:params[:teaching_staff][:qualification],
