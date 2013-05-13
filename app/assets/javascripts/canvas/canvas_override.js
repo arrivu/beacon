3e9525ed1d1aa080955e9bd2cb0304ff004a41f8
sysadmin <sysadmin@sysadmin-ThinkCentre-M58p.(none)>
2013-05-13 15:30:33 +0530

    add canvas styles

---
+++ app/assets/javascripts/canvas/canvas_override.js
@@ -0,0 +1,30 @@
+
+//document.getElementById('logoutbutton').style.display ="none";
+//document.getElementById('course_library_menu_item').style.display ="inline";
+
+function openWin()
+{
+
+	 $.ajax({
+      url: "/logout",
+      type: "POST",   
+      async:false,
+      success: function(){ 
+         success = true
+      }
+    });
+    if(success){ //AND THIS CHANGED
+     window.open("https://beacon.arrivu.corecloud.com/sign_out","_self")
+    }
+
+    
+    	
+}
+$(document).ready(function(){
+  // Identify the logout link in the top menu
+  var logout_button = $('#calendar_menu_item'); 
+  // Create the support button html string
+  var support_button = "<li class='menu-item'><a href='https://beacon.arrivu.corecloud.com/courses' class='menu-item-no-drop'>Course library</a></li>";
+  $(support_button).insertAfter($(logout_button));
+  
+});
