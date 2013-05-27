221637e9c37fda9d80b44baf99bf1f368bc1301f
natarajan <natarajan@arrivusystem.com>
2013-05-27 13:57:23 +0530

    omniauth login keys applied

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, "#{Settings.omniauth.facebook_key}", "#{Settings.omniauth.facebook_secret}"
-  provider :linkedin, "#{Settings.omniauth.linkedin_key}", "#{Settings.omniauth.linkedin_secret}"
-  provider :google_oauth2, "#{Settings.omniauth.google_oauth2_key}", "#{Settings.omniauth.google_oauth2_secret}"
-
+  # provider :facebook, "#{Settings.omniauth.facebook_key}", "#{Settings.omniauth.facebook_secret}"
+  # provider :linkedin, "#{Settings.omniauth.linkedin_key}", "#{Settings.omniauth.linkedin_secret}"
+  # provider :google_oauth2, "#{Settings.omniauth.google_oauth2_key}", "#{Settings.omniauth.google_oauth2_secret}"
   if Rails.env.production?
     provider :facebook, '153576101469926', '302358063a941b9e0d9c8ec9cb77b6ad'
     provider :linkedin, 'z1zu2sg48rvp', "8kpRB8V5EBsiXcbx"
     provider :google_oauth2, '928801233183-phpisd2fada5c2d5n5heosha9h8jisla.apps.googleusercontent.com', 'D6mLnz02jOX6kpT-cbo9p2MM'
   elsif Rails.env.development?
-    provider :facebook, '586163688080266', '093ab151dc5385a434b0e03815ac851f'
+    provider :facebook, '553752584663567', '06f0e8213795d3896e809b8d8eda9332'
     provider :linkedin, '31azcdigi8tt', "OKd1hmuJoNOg9J2g"
     provider :google_oauth2, '928801233183', '-nMQ0dEfcaBITxzwYHZPQ3v-'
   elsif Rails.env.staging?
