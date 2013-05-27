63336ff30a81ccbd77ea60c99a0fbaafdb0baf0b
natarajan <natarajan@arrivusystem.com>
2013-05-27 09:45:49 +0530

    selenium testing

--- spec/selenium/first_test.rb
+++ spec/selenium/first_test.rb
@@ -2,14 +2,16 @@ require 'rubygems'
 require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :firefox
+wait = Selenium::WebDriver::Wait.new()
 
-driver.get "http://localhost:3000/users/sign_in"
+driver.get "http://localhost:3000"
+#wait.until {driver.find_element(:link_text, "Log In")}
+element=driver.find_element(:link_text, "Log In")
+element.click
 
+#wait.until { driver.find_element :id => "user_email" }
 element = driver.find_element :id => "user_email"
 element.send_keys "testdata@gmail.com"
-
-wait = Selenium::WebDriver::Wait.new()
-wait.until { element.send_keys "testdata@gmail.com" }
 
 element = driver.find_element :id =>"user_password"
 element.send_keys "testdata"
@@ -17,11 +19,8 @@ element.submit
 
 puts "Page title is #{driver.title}"
 
-#wait = Selenium::WebDriver::Wait.new(:timeout => 10)
- #wait.until { driver.title.downcase.start_with? "beacon!" }
-
 wait.until{driver.get "http://localhost:3000/courses"}
 puts "Page title is #{driver.title}"
 
- # driver.get "http://localhost:3000/users/sign_out"
- # driver.quit
+ #driver.get "http://localhost:3000/users/sign_out"
+# driver.quit
