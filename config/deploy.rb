1ea46d4a425afcb696f3f7b10c44011531d8aca5
samuel santhosh <samuel@arrivusystems.com>
2013-05-28 16:04:35 +0530

    deploy.rb change

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
-  after "deploy:finalize_update", "deploy:symlink_config", 'deploy:restart'
+  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
@@ -40,5 +40,5 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
-
+after(:deploy, 'deploy:restart')
 after 'deploy:update_code', 'deploy:migrate'
