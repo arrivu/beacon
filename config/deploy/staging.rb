6c0aa51b755ddd7e9c9399d3471d065d4a9fde1a
SaravananNV <saran@arrivusystems.com>
2013-07-15 15:31:30 +0530

    change to deploy/staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@ server "192.168.1.85", :app, :web, :db, :primary => true
 set :deploy_to, "/var/deploy/portal"
 set :rails_env, "staging" 
 set :scm_passphrase, ""
-set :branch, "deploy"
+set :branch, "beackonenewlook"
 set :port, 22
