d1d957596a3293ff93778656171c6844549d7902
SaravananNV <saran@arrivusystems.com>
2013-07-15 15:29:39 +0530

    modify the cap staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-server "192.168.1.54", :app, :web, :db, :primary => true
-set :deploy_to, "/var/deploy/beacon/portal"
+server "192.168.1.85", :app, :web, :db, :primary => true
+set :deploy_to, "/var/deploy/portal"
 set :rails_env, "staging" 
 set :scm_passphrase, ""
 set :branch, "deploy"
