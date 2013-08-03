eaff878e4f45228cf5382e755f6013d502a89e02
franklinjagan <franklinjagan@arrivusystems.com>
2013-08-03 17:09:32 -0400

    override changes

--- app/assets/javascripts/canvas/canvas_override.js
+++ app/assets/javascripts/canvas/canvas_override.js
@@ -1,28 +1,15 @@
-//document.getElementById('logoutbutton').style.display ="none";
-//document.getElementById('course_library_menu_item').style.display ="inline";
 
 function openWin()
 {
-
-	 $.ajax({
-      url: "/logout",
-      type: "POST",
-      async:false,
-      success: function(){
-         success = true
-      }
-    });
-    if(success){ //AND THIS CHANGED
-     window.open("https://online.beaconlearning.in/sign_out","_self")
+  $.ajax({
+    url:"/logout",
+    type:"POST",
+    async:!1,
+    success:function(){
+      success=!0
     }
-
+  }),success&&window.open("https://online.beaconlearning.in/sign_out","_self")
 }
 
 $(document).ready(function(){
-  // Identify the logout link in the top menu
-  var logout_button = $('#calendar_menu_item'); 
-  // Create the support button html string
-  var support_button = "<li class='menu-item'><a href='https://online.beaconlearning.in/my_courses' class='menu-item-no-drop'>My Courses</a></li>";
-  $(support_button).insertAfter($(logout_button));
-  
-});
+  var n=$("#calendar_menu_item"),e="<li class='menu-item'><a href='https://online.beaconlearning.in/my_courses' class='menu-item-no-drop'>My Courses</a></li>";$(e).insertAfter($(n))});
